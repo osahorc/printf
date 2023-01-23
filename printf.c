17cd136340eac57d8b9d33398f3884a8281317ed
Chukwuma Osahor <osahorc@gmail.com>
2023-01-23 11:58:48 -0800

    m

--- printf.c
+++ printf.c
@@ -1,3 +1,11 @@
+/**
+ * _printf - this is the print function
+ * @format: is the format pointer
+ * Description: this is the description
+ *
+ * Return: returns the lenght of the string
+ */
+
 #include "main.h"
 
 int _printf(const char *format, ...)
